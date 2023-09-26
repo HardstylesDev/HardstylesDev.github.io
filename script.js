3a7dc0b3880f116d5329e32e152ef550c52cf062
Tom <getrekttom@gmail.com>
2023-09-26 15:25:17 +0200

    Bye

--- script.js
+++ script.js
@@ -1,93 +1,83 @@
-var textarea = $('.term');
+// Define constants and variables
+const textarea = $('.term');
+let currentIndex = 0;
+let dataIndex = 0;
+const startScript = './welcome.sh';
 
-var i = 0;
-var count = 0;
-
-var start = './welcome.sh'
-var data = [
-    './welcome.sh',
+const scripts = [
+    './start.sh',
     'tom',
     '*********',
     'Yes\n',
     'HardstylesDev',
     'arraylist',
     'hardstyles@proton.me\n\n',
-]
+];
 
-var info = [
+const prompts = [
     "Please state the user you would like to add > ",
     "Please set the password for 'tom' > ",
     "Shall we create a home directory for 'tom' > ",
     "Directory Created, time for your socials!\n\nPlease state your GitHub username, if none put 'N/A' > ",
-    "Please state your Discord username, if none put 'N/A' > ",
-    "Please state your Email, if none put 'N/A' > ",
+    "Please state your Discord username, if none put ' N/A ' > ",
+    "Please state your Email, if none put ' N/A ' > ",
 ];
 
-runner(start)
-
-function runner(text) {
-        if (text) textarea.append(`<span style="color: #a4ffee;">` + text.charAt(i) + `</span>`)
-
-
-    i++;
-    setTimeout(
-        function() {
-            if (i < text.length) {
-                runner(text);
-            } else {
-                // textarea.append("</span>")
-                textarea.append("<br>")
-                i = 0;
-                textarea.append(info[count])
-                count++;
-                runner(data[count])
-                if (count >= data.length) {
-                    setTimeout(function() {feedbacker();}, 650);
-                }
-            }
-        }, Math.floor(Math.random() * 80) + 55)
-}
-
-var count = 0;
-var time = 1;
-
-function feedbacker() {
-    textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
-    if (time % 2 == 0) {
-        i++;
-        textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
-    }
-    if (time == 3) {
-        i++;
-        textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
-        i++;
-        textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
-        i++;
-        textarea.append("[" + count / 1000 + "] " + output[i] + "<br>");
-    }
-    window.scrollTo(0, document.body.scrollHeight);
-    i++;
-    time = Math.floor(Math.random() * 4) + 1;
-    count += time;
-    setTimeout(
-        function() {
-            if (i < output.length - 2)
-                feedbacker();
-            else {
-                textarea.append("<br>Initialising...<br>");
-                setTimeout(function() {$(".load").fadeOut(2000);}, 1000);
-            }
-        },time);
-}
-
-var output = [
+const output = [
     "\nInformation complete! Initializing database.",
     "debug: Creating user account 'tom'",
     "debug: Setting user account password",
     "debug: Creating user home directory, /home/tom",
     "debug: Granting sudo privileges to user 'tom'",
-    "debug: Setting github account name 'HardstylesDev'",
+    "debug: Setting GitHub account name 'HardstylesDev'",
     "debug: Setting Discord username 'hardstylesdev'",
     "debug: Setting email address 'hardstyles@proton.me'",
-    "debug: Account initialized!"
+    "debug: Account initialized!",
 ];
+
+// Function to display text with random delays
+function displayText(text, callback) {
+    if (currentIndex < text.length) {
+        textarea.append(`<span style="color: #c2b0d5;">${text.charAt(currentIndex)}</span>`);
+        currentIndex++;
+        setTimeout(() => displayText(text, callback), Math.floor(Math.random() * 80) + 55);
+    } else {
+        currentIndex = 0;
+        textarea.append("<br>");
+        if (callback) callback();
+    }
+}
+
+// Function to run scripts and prompts
+function runScriptAndPrompt() {
+    if (dataIndex < scripts.length) {
+        displayText(scripts[dataIndex], () => {
+            currentIndex = 0;
+            textarea.append(prompts[dataIndex]);
+            dataIndex++;
+            runScriptAndPrompt();
+        });
+    } else {
+        currentIndex = 0;
+        setTimeout(() => runFeedback(), 650);
+    }
+}
+
+// Function to display feedback
+function runFeedback() {
+    let time = 1;
+    const interval = setInterval(() => {
+        if (currentIndex < output.length) {
+            textarea.append(`[${(currentIndex / 1000).toFixed(3)}] ${output[currentIndex]}<br>`);
+            currentIndex++;
+        } else {
+            clearInterval(interval);
+            currentIndex = 0;
+            textarea.append("<br>Initialising...<br>");
+            setTimeout(() => $(".load").fadeOut(2000), 1000);
+        }
+    }, time);
+}
+
+// Start the process
+runScriptAndPrompt();
